3c35c7a25aad58d24ec9626ace76fbad3ed58dba
yilka <yilkan0510@gmail.com>
2024-08-16 17:41:50 +0200

    used robot to simulate right click. also fixed if the player looks at the air

--- src/main/java/com/github/yilkan0510/chestopener/ChestOpenerMod.java
+++ src/main/java/com/github/yilkan0510/chestopener/ChestOpenerMod.java
@@ -7,7 +7,6 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.MovingObjectPosition.MovingObjectType;
-import net.minecraft.util.Timer;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
@@ -20,6 +19,9 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraft.util.EnumChatFormatting;
 
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
 @Mod(modid = ChestOpenerMod.MODID, version = ChestOpenerMod.VERSION)
 public class ChestOpenerMod {
 
@@ -28,6 +30,15 @@ public class ChestOpenerMod {
     private boolean autoChestOpenEnabled = false;
     private boolean cooldownChest = false;
     private final Minecraft mc = Minecraft.getMinecraft();
+    private Robot robot;
+
+    public ChestOpenerMod() {
+        try {
+            robot = new Robot();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
@@ -64,11 +75,16 @@ public class ChestOpenerMod {
 
                 if (!cooldownChest) {
                     if (mc.theWorld.getBlockState(blockPos).getBlock() == Blocks.chest) {
-                        mc.playerController.onPlayerRightClick(mc.thePlayer, mc.theWorld, mc.thePlayer.getHeldItem(), blockPos, rayTraceResult.sideHit, rayTraceResult.hitVec);
+                        // Simulate right-click action using Robot class
+                        robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+                        robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+                        mc.thePlayer.addChatMessage(new ChatComponentText("chest opened"));
                         cooldownChest = true;
                     }
-
                 }
+            } else {
+                // Reset cooldown if looking at air or other non-block objects
+                cooldownChest = false;
             }
         }
     }
